b6028bd415a6845eb172f3bad57805d1d0c5fe1b
jjoao <jj@di.uminho.pt>
2020-10-26 16:51:14 +0000

    retoques

--- aulas/10-22/makexdxf.py
+++ aulas/10-22/makexdxf.py
@@ -1,31 +1,37 @@
+#!/usr/bin/python3 
+
+from re import *
+import sys
+
 cabecalho = '''<?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE xdxf SYSTEM "https://raw.github.com/soshial/xdxf_makedict/master/format_standard/xdxf_strict.dtd">
 <xdxf lang_from="POR" lang_to="POR" format="logical" revision="033">
     <meta_info>
         <title>Dicionario_medico</title>
-        <full_title>Webster's Unabridged Dictionary</full_title>
-        <description>Webster's Unabridged Dictionary published 1913 by the Webster Institute</description>
+        <full_title>Dicionário Médico</full_title>
+        <description> ... </description>
         <file_ver>001</file_ver>
     </meta_info>
     <lexicon>'''
 
-        #<ar>
-            #<k>entrada</k>
-            #<def>
-                #</def>
-        #</ar>
-from re import *
-import sys
+#entrada :  <ar><k>entrada</k><def></def></ar>
+
 
 def transforma_dic(dic):
 
     with open(dic) as f:
-
         content = f.read() 
-        entradas = split(r"\n{2,}",content)
-        for entrada in entradas:
-            nova = pros(entrada)
-            print(f"<ar>{nova}</ar>")
+
+    ## Limpa
+    content = sub(r'\f','\n',content)
+    content = sub(r'<','&lt;',content) # "<" "&lt;"       
+    content = sub(r'>','&gt;',content) # ">" "&gt"; 
+  
+    ## Anotar artigos, keys, def
+    entradas = split(r"\n{2,}",content)
+    for entrada in entradas:
+        nova = pros(entrada)
+        print(f"<ar>{nova}</ar>")
             
 def pros(entrada):
     entrada = sub(r"(^.*)",r"<k>\1</k><def>",entrada)
