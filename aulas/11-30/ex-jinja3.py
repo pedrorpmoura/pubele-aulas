cf465bce3b0e7667c31eee6579398604d2a4281b
jjoao <jj@di.uminho.pt>
2020-11-30 17:22:06 +0000

    typo...

--- aulas/11-30/ex-jinja3.py
+++ aulas/11-30/ex-jinja3.py
@@ -5,9 +5,9 @@ t = j2.Template( open("musica.template").read() )
 
 
 print(t.render({ 
-   'name' : "experiencia de música",
-   'c' : 'turma de PE',
-   'tit' : '3 galinhas',
-   'vozs' : 'C C G G A A G2 F F E E D D C2',
+   'name' : "Experiência com Jinja2 + música",
+   'c' : 'Turma de PE (versão reduzida)',
+   'tit' : '3 galinhas a cantar',
+   'vozs' : 'C C G G A A G2 | F F E E D D C2 |]',
    'voza': 'C'
    } ))
